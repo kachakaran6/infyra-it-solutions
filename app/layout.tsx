c4c3a583599b26a5fedcac276966a40c12a7d038
kachakaran6 <kachakaran06@gmail.com>
2025-12-17 19:24:01 +0530

    finally icon set

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,11 @@ export const metadata: Metadata = {
   title: "Infyra IT Solutions",
   description:
     "Infyra provides enterprise-grade web development, UI/UX design, automation, and custom software solutions.",
-
-  icons: {
-    icon: "/logo1.png",
-    apple: "/logo1.png",
-  },
+  icons: "/icon1.png",
+  // icons: {
+  //   icon: "/logo1.png",
+  //   apple: "/logo1.png",
+  // },
 };
 
 export default function RootLayout({
